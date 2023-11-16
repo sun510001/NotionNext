b0cc69d332916d700615cd6f13b9a13051d4eada
sun510001 <sqf121@gmail.com>
2023-11-16 15:33:38 +0800

    change font

--- blog.config.js
+++ blog.config.js
@@ -53,7 +53,7 @@ const BLOG = {
 //     '-apple-system',
 //     'BlinkMacSystemFont',
 //     '"Hiragino Sans GB"',
-    '"Microsoft YaHei"',
+//     '"Microsoft YaHei"',
     '"Segoe UI Emoji"',
     '"Segoe UI Symbol"',
     '"Segoe UI"',
