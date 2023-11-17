787423f46855e390f491714970e256a61936163b
sun510001 <sqf121@gmail.com>
2023-11-17 11:17:34 +0800

    change info font style

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -10,7 +10,8 @@ const InfoCard = (props) => {
         <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
         <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
         </div>
-        <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
+        {/* sqf: 修改主页左侧人物信息牌的字体 font-serif -> font-sans*/}
+        <div className='text-2xl font-sans dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
         <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'>{siteConfig('BIO')}</div>
         <SocialButton/>
     </div>
