bef9edbe4bc7f5aa9dd009fb9ce94af2bad3d326
sun510001 <sqf121@gmail.com>
2024-02-02 20:00:23 +0800

    change sitemap page name and reset sitemap settings

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -8,48 +8,43 @@ export const getServerSideProps = async (ctx) => {
   const defaultFields = [
     {
       loc: `${BLOG.LINK}`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }, {
       loc: `${BLOG.LINK}/archive`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
-    }, {
-      loc: `${BLOG.LINK}/sitemap.xml`,
-      changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }, {
       loc: `${BLOG.LINK}/category`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }, {
       loc: `${BLOG.LINK}/feed`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }, {
       loc: `${BLOG.LINK}/search`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }, {
       loc: `${BLOG.LINK}/tag`,
+      lastmod: new Date().toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date().toISOString()
+      priority: '0.7'
     }
   ]
   const postFields = allPages?.filter(p => p.status === BLOG.NOTION_PROPERTY_NAME.status_publish)?.map(post => {
     const slugWithoutLeadingSlash = post?.slug.startsWith('/') ? post?.slug?.slice(1) : post.slug
     return {
       loc: `${BLOG.LINK}/${slugWithoutLeadingSlash}`,
+      lastmod: new Date(post?.publishDay).toISOString().split('T')[0],
       changefreq: 'daily',
-      priority: '0.7',
-      lastmod: new Date(post?.publishDay).toISOString()
+      priority: '0.7'
     }
   })
   const fields = defaultFields.concat(postFields)
